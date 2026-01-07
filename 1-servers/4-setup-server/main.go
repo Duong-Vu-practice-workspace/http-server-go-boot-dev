f1fa9c0f3a7a5163729938e27ead8b34e27baafb
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-07 23:04:07 +0700

    chaper 1 - servers-11-custom-handler

--- 1-servers/4-setup-server/main.go
+++ 1-servers/4-setup-server/main.go
@@ -8,7 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.HandleFunc("/healthz", healthzHandler)
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -16,4 +17,9 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		fmt.Errorf("server error :%v", err)
 	}
+}
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
